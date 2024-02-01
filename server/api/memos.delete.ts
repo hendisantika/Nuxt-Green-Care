92d70d2e82ec719ad8a35c753ee677a1965f9e6b
hendisantika <hendisantika@yahoo.co.id>
2024-02-01 09:01:50 +0700

    Add memos.delete.ts🫸🌀✏️📗🐧🐳

---
+++ server/api/memos.delete.ts
@@ -0,0 +1,25 @@
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+
+    const body = await readBody(event)
+    let memo = null
+    let error = null
+
+    if (body.id)
+        await prisma.memos.delete({
+            where: {
+                id: body.id,
+            },
+        }).then((response) => {
+            memo = response
+        }).catch(async (e) => {
+            error = e
+        })
+
+    if (error) return createError({statusCode: 500, statusMessage: "Server error"});
+
+    return memo
+})
