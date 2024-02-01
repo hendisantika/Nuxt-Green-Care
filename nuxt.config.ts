157e79cb72e18adae151e1429aa56861c9263028
hendisantika <hendisantika@yahoo.co.id>
2024-02-01 09:10:45 +0700

    Add common.css🫸🌀✏️📗🐧🐳

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: {enabled: true}
+    devtools: {enabled: true},
+    css: [
+        '~/assets/scss/common.scss'
+    ],
+    serverMiddleware: [
+        {path: '/api', handler: '~/api/plants.js'},
+    ],
 })
