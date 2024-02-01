25cea9d202171fa9e60f8a3f6fbdf0f85ed6abab
hendisantika <hendisantika@yahoo.co.id>
2024-02-01 09:08:39 +0700

    Add memos.put.ts🫸🌀✏️📗🐧🐳

---
+++ server/api/memos.put.ts
@@ -0,0 +1,26 @@
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+
+    const body = await readBody(event)
+    const id = body.id
+    const content = body.content
+
+    if (!(id && content)) return createError({statusCode: 400, statusMessage: "Missing id and content"});
+
+    let memo = null
+
+    if (id && content)
+        memo = await prisma.memos.update({
+            where: {
+                id: id,
+            },
+            data: {
+                content: content,
+            },
+        })
+
+    return memo
+})
