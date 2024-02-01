1b184709f2c69332778077db056d1702c1b79c4a
hendisantika <hendisantika@yahoo.co.id>
2024-02-01 09:07:49 +0700

    Add memos migration🫸🌀✏️📗🐧🐳

---
+++ prisma/migrations/20240201020255_init_memo/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `memos`
+(
+    `id`         INTEGER      NOT NULL AUTO_INCREMENT,
+    `content`    VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP (0),
+    `updated_at` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
