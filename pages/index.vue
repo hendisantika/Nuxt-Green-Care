02e80861a36aa8e7a6ae11548ad22f10aa77d06d
hendisantika <hendisantika@yahoo.co.id>
2024-02-01 08:45:36 +0700

    Modify index.vue 🫸🌀✏️📗🐧🐳

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,17 @@
 <template>
   <h1>Top Page</h1>
 </template>
+
+<script>
+useHead({
+  title: "Green Care",
+  link: {
+    rel: "stylesheet",
+    href: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
+    type: "text/css",
+  },
+  script: {
+    src: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js",
+  },
+});
+</script>
