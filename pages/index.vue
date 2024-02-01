6e8e8426120e131645aefee2c08b0abc62c351c5
hendisantika <hendisantika@yahoo.co.id>
2024-02-01 08:46:04 +0700

    Add Bootstrap UI 🫸🌀✏️📗🐧🐳

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,40 @@
 <template>
-  <h1>Top Page</h1>
+  <div class="container">
+    <h1>Top Page</h1>
+    <table class="table">
+      <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Edit</th>
+        <th scope="col">Delete</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(plant, index) in plants">
+        <th scope="row">{{ index + 1 }}</th>
+        <td>{{ plant.name }}</td>
+        <td>
+          <button class="btn btn-warning btn-sm" type="button">Edit</button>
+        </td>
+        <td>
+          <button class="btn btn-danger btn-sm" type="button">Delete</button>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
-<script>
+<script setup>
+const plants = ref(null);
+plants.value = await getPlants();
+
+// Get plants
+async function getPlants() {
+  return await $fetch("/api/plants");
+}
+
 useHead({
   title: "Green Care",
   link: {
