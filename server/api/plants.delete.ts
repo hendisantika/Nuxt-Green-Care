88070c8cd176075d6afa3756c5fde3bf33109bf6
hendisantika <hendisantika@yahoo.co.id>
2024-02-01 08:44:45 +0700

    Modify plants.delete.ts 🫸🌀✏️📗🐧🐳

--- server/api/plants.delete.ts
+++ server/api/plants.delete.ts
@@ -1,5 +1,25 @@
-export default defineEventHandler((event) => {
-    return {
-        api: 'delete plants'
-    }
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+
+    const body = await readBody(event)
+    let plant = null
+    let error = null
+
+    if (body.id)
+        await prisma.plants.delete({
+            where: {
+                id: body.id,
+            },
+        }).then((response) => {
+            plant = response
+        }).catch(async (e) => {
+            error = e
+        })
+
+    if (error) return createError({statusCode: 500, statusMessage: "Server error"});
+
+    return plant
 })
