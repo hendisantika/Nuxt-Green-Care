cdaea69d50b00117fb64e0dc9c97dcd935b2f54f
hendisantika <hendisantika@yahoo.co.id>
2024-02-01 08:44:25 +0700

    Modify plants.get.ts 🫸🌀✏️📗🐧🐳

--- server/api/plants.get.ts
+++ server/api/plants.get.ts
@@ -1,5 +1,7 @@
-export default defineEventHandler((event) => {
-    return {
-        api: 'read plants'
-    }
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async () => {
+    return await prisma.plants.findMany()
 })
