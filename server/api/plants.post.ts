ed020acfa4f736acaa5fd28742da797fc64291d0
hendisantika <hendisantika@yahoo.co.id>
2024-02-01 08:44:04 +0700

    Modify plants.post.ts 🫸🌀✏️📗🐧🐳

--- server/api/plants.post.ts
+++ server/api/plants.post.ts
@@ -1,5 +1,22 @@
-export default defineEventHandler((event) => {
+import {PrismaClient} from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default defineEventHandler(async (event) => {
+
+    const body = await readBody(event)
+    let plant = null
+
+    if (body.name)
+        await prisma.plants.create({
+            data: {
+                name: body.name,
+            },
+        }).then((response) => {
+            plant = response
+        })
+
     return {
-        api: 'create plants'
+        plant: plant
     }
 })
